c6dc53ccde54631bc39eb2045dc290784f51fcaa
javierbelogarcia <javier.belo@outlook.com>
2017-09-12 18:12:29 +0200

    performed basic changes

--- atom_catastro.py
+++ atom_catastro.py
@@ -13,13 +13,16 @@ import csv
 import threading
 
 ATOM_DATA = {'addresses': {'url': "http://www.catastro.minhap.es/INSPIRE/Addresses/ES.SDGC.AD.atom.xml",
-                               'folder': "/addresses"},
+                               'folder': "addresses"},
                       'parcels': {'url': "http://www.catastro.minhap.es/INSPIRE/CadastralParcels/ES.SDGC.CP.atom.xml",
-                                   'folder': "/parcels"},
+                                   'folder': "parcels"},
                       'buildings': {'url': "http://www.catastro.minhap.es/INSPIRE/buildings/ES.SDGC.BU.atom.xml",
-                                     'folder': "/buildings"}}
+                                     'folder': "buildings"}}
 PATH_ROOT = "/Users/javierbelogarcia/Documents/catastro"
 
+
+class WorkingException(Exception):
+    pass
 
 class CSVHandler:
 
@@ -39,7 +42,7 @@ class CSVHandler:
         if self.csv_log is None:
             self.csv_log = open(path_folder + '/errors.csv', 'wb')
 
-    def write_csv(self, rows: []) -> None:
+    def write_csv(self, rows: list) -> None:
         """
         Update the csv with the rows passed as argument
         :param rows: array containing the rows to include in the csv
@@ -56,7 +59,8 @@ class HardWorker:
     def __init__(self):
         pass
 
-    def folder_setting(self, path_root: str, folder: str) -> str:
+    @staticmethod
+    def folder_setting(path_root: str, folder: str) -> str:
         """
         Set up directory, in case directory exist raise an exception. If path is created successfully then
         the string of path directory is returned
@@ -70,66 +74,125 @@ class HardWorker:
         return path_folder
         pass
 
-    def retrieve(self,atom_url: str, path_folder: str) -> []:
+    @staticmethod
+    def entries_list(atom_url: str) -> list:
+        atom = fp(atom_url)
+        entries = atom.entries
+        return entries
+
+    @staticmethod
+    def level_reader(atom_url: str, path_folder: str) -> list:
         """
-        Iterate parse entries and download zip files with Catastro data of each municipality
         :param atom_url: url of Catastro atom service
         :param path_folder: directory where Catastro data will be downloaded
-        :return: array with errors in municipality downloading
+        :return: list with errors in municipality downloading
+        """
+        # amount of entries at first level. In
+        entries_data = []
+        folder_entry = None
+        path_folder_entry = None
+        sub_atom = None
+        try:
+            atom = fp(atom_url)
+            entries = atom.entries
+            for entry in entries:
+                # changing the names
+                m_pos = [pos for pos, char in enumerate(entry.title) if char == " "]
+                folder_entry = entry.title[m_pos[1] + 1:]
+                folder_entry = folder_entry.replace(' ', '_')
+                path_folder_entry = path_folder + '/' + folder_entry
+                sub_atom = entry.enclosures[0].href
+                entries_data.append({'folder': folder_entry, 'path': path_folder_entry, 'reference': sub_atom})
+                # raise FileExistsError if the directory already exist. avoid overwriting
+                #os.mkdir(path_folder_entry)
+                # enter in closure
+        except Exception as e:
+            print("Exception exception: " + str(e))
+        return entries_data
+
+
+    @staticmethod
+    def extractor(atom_url: str, file_type: str = 'application/atom+xml') -> list:
+        """
+        :param atom_url: url of Catastro atom service
+        :param file_type:
+        º:return: list with errors in municipality downloading
+        """
+        entries_file = []
+        try:
+            atom = fp(atom_url)
+            entries = atom.entries
+            for entry in entries:
+                if entry.enclosures[0].type == file_type:
+                    entries_file.append(entry)
+                # raise FileExistsError if the directory already exist. avoid overwriting
+        except Exception as e:
+            raise WorkingException('Problems extracting list of values')
+        return entries_file
+
+    @staticmethod
+    def downloader(lvl_name: str, entries: list, path_folder: str, type: str = 'application/zip') -> list:
+        """
+        Iterate parse entries and download zip files with Catastro data of each municipality
+        :param lvl_name: name of the level for this list. In this case Catastro province
+        :param entries: list of entries containing file for downloading
+        :param path_folder: directory where Catastro data will be downloaded
+        :param type: type of file to download
+        :return: list with errors in municipality downloading
         """
         # multidimensional array containing the errors generated during the downloading process
         failures = []
-        # amount of entries at first level. In Catastro means provinces amount
-        total_entries_lvl_0 = 0
-        # atom entries processed at first level. In Catastro means provinces
-        entries_lvl_0 = 0
         # atom entries at second level. In Catastro means municipalities
-        entries_lvl_1 = 0
+        entries_downloaded = 0
+        total_entries = len(entries)
+        file_extension = None
+        if type == 'application/zip':
+            file_extension = '.zip'
         try:
-            atom = fp(atom_url)
-            entries_first_lvl = atom.entries
-            total_entries_lvl_0 = len(entries_first_lvl)
-            print('Total provinces: {}'.format(total_entries_lvl_0))
-            # iterate the initial entries
-            for entry in entries_first_lvl:
-                # changing the names
-                m_pos = [pos for pos, char in enumerate(entry.title) if char == " "]
-                folder_entry = entry.title[m_pos[1]+1:]
-                folder_entry = folder_entry.replace(' ','_')
-                path_folder_entry = path_folder + '/' + folder_entry
-                # raise FileExistsError if the directory already exist. avoid overwriting
-                os.mkdir(path_folder_entry)
-                # enter in closure
-                sub_a = fp(entry.enclosures[0].href)
-                entries_second_lvl = sub_a.entries
-                for entry_n in entries_second_lvl:
-                    file_name = entry_n.title.rsplit(' ', 1)[0]
-                    file_name = file_name.replace(' ', '_')
-                    path_file = path_folder_entry + '/' + file_name + '.zip'
-                    href = entry_n.enclosures[0].href
-                    url_enclosure = href.replace('http://', '')
-                    url_enclosure = urllib.parse.quote(url_enclosure)
-                    try:
-                        urllib.request.urlretrieve('http://'+url_enclosure, path_file)
-                    # TODO handle exceptions raised by urllib.request...
-                    except Exception as e:
-                        failures.append([folder_entry, file_name, str(e)])
-                    entries_lvl_1 += 1
-                entries_lvl_0 += 1
-                print('Province: ' + folder_entry + '  Municipalities: ' + str(entries_lvl_1) +
-                      '  Provinces downloaded: ' + str(entries_lvl_0) + '/' + str(total_entries_lvl_0))
-                entries_lvl_1 = 0
-                # if provinces == 5:
-                    # break
+            # TODO review if changing by makedirs
+            # raise FileExistsError if the directory already exist. avoid overwriting
+            os.mkdir(path_folder)
+            for entry_n in entries:
+                file_name = entry_n.title.rsplit(' ', 1)[0]
+                file_name = file_name.replace(' ', '_')
+                path_file = path_folder + '/' + file_name + file_extension
+                href = entry_n.enclosures[0].href
+                url_enclosure = href.replace('http://', '')
+                url_enclosure = urllib.parse.quote(url_enclosure)
+                try:
+                    urllib.request.urlretrieve('http://'+url_enclosure, path_file)
+                    entries_downloaded += 1
+                # TODO handle exceptions raised by urllib.request...
+                except Exception as e:
+                    failures.append([lvl_name, file_name, str(e)])
+
+            print('Province: ' + lvl_name + '  Municipalities downloaded: ' + str(entries_downloaded) + '/' +
+                  str(total_entries))
+            return failures
+            # if provinces == 5:
+                # break
+        # TODO review how to manage these exception
         except FileExistsError as e:
-            print("Exception FileExistsError: " + str(e))
+            print("Exception FileExistError: " + str(e))
         except Exception as e:
             print("Exception exception: " + str(e))
-        return failures
 
 
 def main():
-    print("main function")
+
+    try:
+        # 1 make directory
+        path_to_folder = HardWorker.folder_setting(PATH_ROOT,ATOM_DATA['addresses']['folder'])
+        list_entries = HardWorker.level_reader(ATOM_DATA['addresses']['url'], path_to_folder)
+        for a in list_entries:
+            # print(a['folder'], a['path'], a['reference'])
+            list_files = HardWorker.extractor(a['reference'])
+            # for b in list_files:
+                # print(b.title)
+    except OSError as e:
+        print("Exception OSError: " + str(e))
+    except WorkingException as e:
+        print("Exception WorkingException: " + str(e))
 
 
 if __name__ == '__main__':
